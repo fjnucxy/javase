be4ea7196a6edbf7fd9c50b3b2ef850b243f9dcf
cxy <1578803466@qq.com>
2024-05-17 20:58:38 +0800

    flow-control

--- basic-app/src/com/itheima/scanner/ScannerDemo1.java
+++ basic-app/src/com/itheima/scanner/ScannerDemo1.java
@@ -7,7 +7,6 @@ public class ScannerDemo1 {
         // 1、导包:一般不需要我们自己做，idea工具会自动帮助我们 导包的。
         // 2、抄写代码：得到一个键盘扫描器对象（东西）
         Scanner sc = new Scanner(System.in);
-
         // 3、开始 调用sc的功能，来接收用户键盘输入的数据。
         System.out.println("请您输入您的年龄：");
         int age = sc.nextInt(); // 执行到这儿，会开始等待用户输入一个整数，直到用户按了回车键，才会拿到数据。
